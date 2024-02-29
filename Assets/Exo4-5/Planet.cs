50b0d20f9915cbabe338fac60df3c13fad44c636
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2024-02-29 07:34:06 +0100

    Ex5

--- Assets/Exo4-5/Planet.cs
+++ Assets/Exo4-5/Planet.cs
@@ -1,8 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    
+    public GameObject TextUI;
+    public int Population;
+
+    [SerializeField] TextMeshProUGUI _populationText;
+    [SerializeField] GameObject[] _otherPlanets;
+    [SerializeField] GameObject _rocketPrefab;
+
+    private void Start()
+    {
+        TextUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        Population += (int)(Random.Range(0, 1000) * Time.deltaTime);
+
+        _populationText.text = Population.ToString();
+
+        var number = Random.Range(0, 10000);
+
+        if (number == 0)
+        {
+            LaunchRocket();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        for (int i = 0; i < _otherPlanets.Length; i++)
+        {
+            _otherPlanets[i].GetComponent<Planet>().TextUI.SetActive(false);
+        }
+
+        TextUI.SetActive(!TextUI.activeSelf);
+    }
+
+    void LaunchRocket()
+    {
+        var rocket = Instantiate(_rocketPrefab, transform.position, Quaternion.identity);
+        rocket.GetComponent<Rocket>().GoFrom = this;
+
+        var number = Random.Range(0, _otherPlanets.Length);
+
+        rocket.GetComponent<Rocket>().GoFrom = this;
+        rocket.GetComponent<Rocket>().GoTo = _otherPlanets[number].GetComponent<Planet>();
+    }
 }
--- Assets/Exo4-5/Rocket.cs
+++ Assets/Exo4-5/Rocket.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    
+    public Planet GoFrom;
+    public Planet GoTo;
+    [SerializeField] int _pilots;
+    [SerializeField] bool _willExplode;
+
+    private void Start()
+    {
+        _pilots = (int)(GoFrom.Population / 2);
+
+        if (_pilots < 2)
+            Destroy(gameObject);
+
+        GoFrom.Population -= _pilots;
+
+        int number = Random.Range(0, 10);
+
+        if (number == 0)
+            _willExplode = true;
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, GoFrom.transform.position - GoTo.transform.position);
+        transform.Translate(Vector3.down * 1 * Time.deltaTime);
+
+        if (transform.position == GoTo.transform.position - GoFrom.transform.position)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Planet>() != null && other.GetComponent<Planet>() == GoTo)
+        {
+            GoTo.Population += _pilots;
+            Destroy(gameObject);
+        }
+    }
 }
